2fe5ed970fa8d9dec87ae288395da4c6e79ed7d3
michaelrauh <michaelrauh@ymail.com>
2020-10-31 11:37:56 -0400

    introduce helper to build sliding lists instead of hard coding

--- search.rkt
+++ search.rkt
@@ -1,7 +1,13 @@
 #lang rosette/safe
 
+(define (build-sliding xs)
+  (cond
+    [(empty? xs) (list)]
+    [(= 1 (length xs)) (list)]
+    [else (cons (take xs 2) (build-sliding (drop xs 1)))]))
+
 (define example (list 1 2 3 4 1 3 2 4))
-(define example-sliding (list (list 1 2) (list 2 3) (list 3 4) (list 4 1) (list 1 3) (list 3 2) (list 2 4)))
+(define example-sliding (build-sliding example))
 (define-symbolic a b c d integer?)
 (define xs (list a b))
 (define sol (solve (begin
@@ -10,4 +16,5 @@
                      (assert (member (list a c) example-sliding))
                      (assert (member (list b d) example-sliding))
                      (assert (not (equal? b c))))))
+
 (evaluate (list (list a b) (list c d)) sol)
