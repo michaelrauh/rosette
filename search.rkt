5c0529375128270332590183184f601faea165bb
michaelrauh <michaelrauh@ymail.com>
2020-10-31 08:00:07 -0400

    extract var to hold symbolic integers

--- search.rkt
+++ search.rkt
@@ -1,5 +1,6 @@
 #lang rosette
 
 (define-symbolic x y integer?)
-(define sol (solve (assert (member (list x y) (list (list 1 2) (list 2 3))))))
-(evaluate (list x y) sol)
+(define xs (list x y))
+(define sol (solve (assert (member xs (list (list 1 2) (list 2 3))))))
+(evaluate xs sol)
