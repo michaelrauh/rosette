b812f38917817a040d952b3c90b9fc49371e0219
michaelrauh <michaelrauh@ymail.com>
2020-10-31 09:35:42 -0400

    show hard coded mechanics for asserting text overlaps

--- search.rkt
+++ search.rkt
@@ -1,6 +1,13 @@
 #lang rosette
 
-(define-symbolic x y integer?)
-(define xs (list x y))
-(define sol (solve (assert (member xs (list (list 1 2) (list 2 3))))))
-(evaluate xs sol)
+(define example (list 1 2 3 4 1 3 2 4))
+(define example-sliding (list (list 1 2) (list 2 3) (list 3 4) (list 4 1) (list 1 3) (list 3 2) (list 2 4)))
+(define-symbolic a b c d integer?)
+(define xs (list a b))
+(define sol (solve (begin
+                     (assert (member (list a b) example-sliding))
+                     (assert (member (list c d) example-sliding))
+                     (assert (member (list a c) example-sliding))
+                     (assert (member (list b d) example-sliding))
+                     (assert (not (equal? b c))))))
+(evaluate (list (list a b) (list c d)) sol)
