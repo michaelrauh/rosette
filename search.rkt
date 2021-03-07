bae8d50fa4c262322db02f633eca638d646a6e98
michaelrauh <michaelrauh@ymail.com>
2021-03-06 20:27:59 -0500

    make symbols list

--- search.rkt
+++ search.rkt
@@ -3,14 +3,18 @@
 (require "helper.rkt")
 (require math)
 
+(define (create-symbol)
+ (define-symbolic* x integer?) x)
+
 (define-symbolic a b c d e f g h integer?)
+(define symbols (list a b c d e f g h))
 
-(define arr (array-reshape (array #[a b c d e f g h]) #(2 2 2))) ; note: replace array with n calls to define-symbolic*. Replace dims with passed in dims
+(define arr (array-reshape (list->array symbols) #(2 2 2))) ; replace array with n calls to define-symbolic*. Replace dims with passed in dims
 
-(define p1 (array-axis-swap arr 1 2)) ; note: investigate how these permutations progress for higher dimensions
+(define p1 (array-axis-swap arr 1 2)) ; investigate how these permutations progress for higher dimensions
 (define p2 (array-axis-swap arr 0 2))
 
-(define a1 (array->list* (array-reshape arr #(4 2))))
+(define a1 (array->list* (array-reshape arr #(4 2)))) ; in this case 2 is the length of the target dimension, and 4 is what is left over. This will be different on each line if it's not square.
 (define a2 (array->list* (array-reshape p1 #(4 2))))
 (define a3 (array->list* (array-reshape p2 #(4 2))))
 
@@ -18,14 +22,8 @@
   (for-each (λ (x) (assert (tree-member? x l))) a1)
   (for-each (λ (x) (assert (tree-member? x l))) a2)
   (for-each (λ (x) (assert (tree-member? x l))) a3)
-  (for-each (λ (x) (assert (not (equal? (first x) (second x))))) (list (list b c) (list b e) (list d g) (list f g)))) ; note: calculate this instead of hard code
+  (for-each (λ (x) (assert (not (equal? (first x) (second x))))) (list (list b c) (list b e) (list d g) (list f g)))) ; calculate this instead of hard code
 (define sol (solve (check (build-example-sliding 2))))
 
-(define answer (evaluate (list a b c d e f g h) sol))
+(define answer (evaluate symbols sol))
 (convert-back answer)
-
-; a b   e f
-; c d   g h
-
-; 0 1   1 2
-; 1 2   2 3
