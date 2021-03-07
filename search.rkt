5174a3de0efe7b141b79439d900e5c2fab0bb619
michaelrauh <michaelrauh@ymail.com>
2021-03-07 14:09:43 -0500

    collapse swapped dimensions down into a single calculation. Still need to calculate the flatten operation dimensions

--- search.rkt
+++ search.rkt
@@ -7,17 +7,17 @@
 
 (define (create-symbol)
  (define-symbolic* x integer?) x)
-
-(define symbols (map (λ (_) (create-symbol)) (range (apply * dims))))
+(define volume (apply * dims))
+(define dimensionality (- (length dims) 1))
+(define symbols (map (λ (_) (create-symbol)) (range volume)))
 
 (define arr (array-reshape (list->array symbols) (list->vector dims)))
 
-(define p1 (array-axis-swap arr 1 2)) ; investigate how these permutations progress for higher dimensions
-(define p2 (array-axis-swap arr 0 2))
+(define ps (map (λ (ax) (array-axis-swap arr ax dimensionality)) (range (+ 1 dimensionality))))
 
-(define a1 (array->list* (array-reshape arr #(4 2)))) ; in this case 2 is the length of the target dimension, and 4 is what is left over. This will be different on each line if it's not square.
-(define a2 (array->list* (array-reshape p1 #(4 2))))
-(define a3 (array->list* (array-reshape p2 #(4 2))))
+(define a1 (array->list* (array-reshape (list-ref ps 0) #(4 2)))) ; in this case 2 is the length of the target dimension, and 4 is what is left over. This will be different on each line if it's not square.
+(define a2 (array->list* (array-reshape (list-ref ps 1) #(4 2))))
+(define a3 (array->list* (array-reshape (list-ref ps 2) #(4 2))))
 
 (define (check l)
   (for-each (λ (x) (assert (tree-member? x l))) a1)
@@ -27,23 +27,4 @@
 (define sol (solve (check (build-example-sliding 2))))
 
 (define answer (evaluate symbols sol))
-;(convert-back answer)
-
-
-(define dims2 '(2 2 2 2))
-(define ex (range (apply * dims2)))
-
-(define arr2 (array-reshape (list->array ex) (list->vector dims2)))
-arr2
-
-(array-axis-swap arr2 0 3)
-(array-axis-swap arr2 1 3)
-(array-axis-swap arr2 2 3)
-(array-axis-swap arr2 3 3)
-
-; 0 1   4 5
-; 2 3   6 7
-
-
-; 8  9   12 13
-; 10 11  14 15
+(convert-back answer)
