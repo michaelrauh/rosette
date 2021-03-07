7944eb8e0108b4b8f451641906c32d9c2290ad0b
michaelrauh <michaelrauh@ymail.com>
2021-03-07 15:34:11 -0500

    collapse into nested loop

--- search.rkt
+++ search.rkt
@@ -18,9 +18,10 @@
 (define phrases (map (λ (pos) (array->list* (array-reshape (list-ref all_orthotope_views pos) (list->vector (list (/ volume (list-ref dims pos)) (list-ref dims pos))))))  (range (+ 1 dimensionality))))
 
 (define (check)
-  (for-each (λ (x) (assert (tree-member? x (build-example-sliding (list-ref dims 0))))) (list-ref phrases 0))
-  (for-each (λ (x) (assert (tree-member? x (build-example-sliding (list-ref dims 1))))) (list-ref phrases 1))
-  (for-each (λ (x) (assert (tree-member? x (build-example-sliding 2)))) (list-ref phrases 2))
+  (for-each (λ (pos)
+   (for-each (λ (x) (assert (tree-member? x (build-example-sliding (list-ref dims pos))))) (list-ref phrases pos)))
+            (range (+ 1 dimensionality)))
+   
   (for-each (λ (x) (assert (not (equal? (first x) (second x))))) (list (list (list-ref symbols 1) (list-ref symbols 2)) (list (list-ref symbols 1) (list-ref symbols 4)) (list (list-ref symbols 3) (list-ref symbols 7)) (list (list-ref symbols 5) (list-ref symbols 7))))) ; calculate this instead of hard code
 (define sol (solve (check)))
 
