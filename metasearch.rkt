3748765e89e6802c06604ae46d7d6537d6fa795f
michaelrauh <michaelrauh@ymail.com>
2021-03-10 17:01:21 -0500

    add more to metasearch to find largest object. Rely on side effects to produce objects and report failure for now. Still hard coding input path.

--- metasearch.rkt
+++ metasearch.rkt
@@ -1,14 +1,33 @@
 #lang racket
 (require "search.rkt")
 
-(struct search-res (res dims))
-
 (define (find-dimensionality)
-  (define (go acc)
-    (define ans (search (search-res-dims acc)))
+  (define (go dims)
+    (define ans (search dims))
     (if (not ans)
-        acc
-        (go (search-res ans (cons 2 (search-res-dims acc))))))
-  (go (search-res #f '(2 2))))
+        dims
+        (go (cons 2 dims))))
+  (cdr (go '(2 2))))
 
 (define dimensionality (find-dimensionality))
+
+(define (find-shape dims cur)
+  (define failed  (not (search dims)))
+  (define at-end (= cur (sub1 (length dims))))
+  (cond
+    [(and failed at-end) (unbump dims cur)]
+    [failed (find-shape (bump (unbump dims cur) (add1 cur)) (add1 cur))]
+    [else (find-shape (bump dims cur) cur)]))
+      
+(define (bump l pos)
+  (list-update l pos add1))
+
+(define (unbump l pos)
+  (list-update l pos sub1))
+
+(define (metasearch)
+  (if (> (length dimensionality) 1)
+      (find-shape (bump dimensionality 0) 0)
+      (display "zero interesting things found")))
+
+(metasearch)
