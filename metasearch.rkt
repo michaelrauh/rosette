95f3ee3283d9db11492494b95da9e5ec5f5196fc
michaelrauh <michaelrauh@ymail.com>
2021-03-11 21:52:56 -0500

    new ideas on searching each dimensionality before expanding dimensions. Had to introduce looking back warning bit.

--- metasearch.rkt
+++ metasearch.rkt
@@ -11,13 +11,14 @@
 
 (define dimensionality (find-dimensionality))
 
-(define (find-shape dims cur)
+(define (find-shape dims cur jumped)
   (define failed  (not (search dims)))
   (define at-end (= cur (sub1 (length dims))))
   (cond
+    [(and failed jumped) (unbump dims cur)]
     [(and failed at-end) (unbump dims cur)]
-    [failed (find-shape (bump (unbump dims cur) (add1 cur)) (add1 cur))]
-    [else (find-shape (bump dims cur) cur)]))
+    [failed (find-shape (bump (unbump dims cur) (add1 cur)) (add1 cur) #t)]
+    [else (find-shape (bump dims cur) cur #f)]))
       
 (define (bump l pos)
   (list-update l pos add1))
@@ -27,7 +28,19 @@
 
 (define (metasearch)
   (if (> (length dimensionality) 1)
-      (find-shape (bump dimensionality 0) 0)
+      (find-shape (bump dimensionality 0 #t) 0)
       (display "zero interesting things found")))
 
 (metasearch)
+
+(define (combined-search dims cur jumped)
+  (define failed (not (search dims)))
+  (define at-end (= cur (sub1 (length dims))))
+  (cond
+    [(and failed jumped) (unbump dims cur)]
+    [(and failed at-end) (combined-search ((make-list (add1 (length dims)) 2) dims) 0 #t)]
+    [failed (combined-search (bump (unbump dims cur) (add1 cur)) (add1 cur) #t)]
+    [else (combined-search (bump dims cur) cur #f)]))
+
+(define (new-search)
+  (combined-search '(2 2) 0 #t))
