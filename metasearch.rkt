843720be0e1a3ecb2e531a0d4c309424218b3acd
michaelrauh <michaelrauh@ymail.com>
2021-03-11 22:19:56 -0500

    fix to short circuit - new logic skips to add dimension if a failure is found after changing dimensions

--- metasearch.rkt
+++ metasearch.rkt
@@ -12,6 +12,7 @@
   (define at-end (= cur (sub1 (length dims))))
   (cond
     [(and failed new-dim) (begin (displayln "failed after new dimension added.") (cdr dims))]
+    [(and failed new-cur) (begin (displayln "failed after changing dimension. Skipping to add dimension") (combined-search (make-list (add1 (length dims)) 2) 0 #f #t))]
     [(and failed at-end) (begin (displayln "got to end of dimension. adding new.") (combined-search (make-list (add1 (length dims)) 2) 0 #f #t))]
     [failed (begin (displayln "failed on current. Moving to new dimension.") (combined-search (bump (unbump dims cur) (add1 cur)) (add1 cur) #t #f))]
     [else (begin (displayln "succeeded. Adding to dimension.") (combined-search (bump dims cur) cur #f #f))]))
